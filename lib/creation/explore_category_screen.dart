77369b74568a91029c630cd7c760d1b451a338a1
Abdullah <abdullahishaq2310@gmail.com>
2026-02-11 16:49:09 +0500

    ko

--- lib/creation/explore_category_screen.dart
+++ lib/creation/explore_category_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../l10n/app_localizations.dart';
 import '../utils/colors.dart';
 import 'explore_detail_screen.dart';
 
@@ -21,7 +20,6 @@ class ExploreCategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
     final isDark = Theme.of(context).brightness == Brightness.dark;
     final textColor = isDark ? AppColors.textWhite : AppColors.textPrimary;
 
@@ -37,8 +35,9 @@ class ExploreCategoryScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:
-            isDark ? AppColors.darkBackground : AppColors.lightBackground,
+        backgroundColor: isDark
+            ? AppColors.darkBackground
+            : AppColors.lightBackground,
         foregroundColor: textColor,
         elevation: 0,
         title: Text(
@@ -50,43 +49,28 @@ class ExploreCategoryScreen extends StatelessWidget {
           ),
         ),
       ),
-      backgroundColor:
-          isDark ? AppColors.darkBackground : AppColors.lightBackground,
+      backgroundColor: isDark
+          ? AppColors.darkBackground
+          : AppColors.lightBackground,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              l10n.homeExploreInspiration,
-              style: TextStyle(
-                fontSize: 16.sp,
-                fontWeight: FontWeight.w400,
-                color: textColor.withOpacity(0.8),
-              ),
-            ),
-            SizedBox(height: 12.h),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 12.h,
-                  crossAxisSpacing: 12.w,
-                  childAspectRatio: 0.8,
-                ),
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  final item = items[index];
-                  return _CategoryDetailCard(
-                    title: item['title']!,
-                    prompt: item['prompt']!,
-                    bigImagePath: item['bigImage']!,
-                    smallImagePath: item['smallImage'],
-                  );
-                },
-              ),
-            ),
-          ],
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 12.h,
+            crossAxisSpacing: 12.w,
+            childAspectRatio: 0.8,
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return _CategoryDetailCard(
+              title: item['title']!,
+              prompt: item['prompt']!,
+              bigImagePath: item['bigImage']!,
+              smallImagePath: item['smallImage'],
+            );
+          },
         ),
       ),
     );
@@ -126,67 +110,31 @@ class _CategoryDetailCard extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12.r),
-          border: Border.all(
-            color: const Color(0xFFFE8B3A),
-            width: 1.5.w,
-          ),
-          color: isDark ? AppColors.cardGradientStart : AppColors.lightBackground,
+          border: Border.all(color: const Color(0xFFFE8B3A), width: 1.5.w),
+          color: isDark
+              ? AppColors.cardGradientStart
+              : AppColors.lightBackground,
         ),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(12.r),
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Image.asset(
-                  bigImagePath,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      color: AppColors.cardGradientStart,
-                      child: Center(
-                        child: Icon(
-                          Icons.image_not_supported,
-                          color: AppColors.textGrey,
-                          size: 24.sp,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  width: double.infinity,
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 8.w, vertical: 6.h),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.black.withOpacity(0.0),
-                        Colors.black.withOpacity(0.6),
-                      ],
-                    ),
-                  ),
-                  child: Text(
-                    title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 12.sp,
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.textWhite,
-                    ),
+          child: Image.asset(
+            bigImagePath,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return Container(
+                color: AppColors.cardGradientStart,
+                child: Center(
+                  child: Icon(
+                    Icons.image_not_supported,
+                    color: AppColors.textGrey,
+                    size: 24.sp,
                   ),
                 ),
-              ),
-            ],
+              );
+            },
           ),
         ),
       ),
     );
   }
 }
-
